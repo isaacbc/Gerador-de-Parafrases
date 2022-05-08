8bd9869b704accb38278b8b7decf24ca567488d0
Isaac Bueno <isaacbc42@gmail.com>
2022-05-07 21:50:57 -0300

    - add binTree project

--- BinTree/binTree.h
+++ BinTree/binTree.h
@@ -1,20 +1,19 @@
 typedef int tipoinfo;
 
-struct TNodoA
-{
-  tipoinfo info;
-  struct TNodoA *esq;
-  struct TNodoA *dir;
+struct TNodoA{
+        tipoinfo info;
+        struct TNodoA *esq;
+        struct TNodoA *dir;
 };
 
 typedef struct TNodoA pNodoA;
 
-pNodoA *InsereArvore(pNodoA *a, tipoinfo ch);
+pNodoA* InsereArvore(pNodoA *a, tipoinfo ch);
 void preFixadoE(pNodoA *a);
 void preFixadoD(pNodoA *a);
 void posFixadoE(pNodoA *a);
 void posFixadoD(pNodoA *a);
 void CentralE(pNodoA *a);
 void CentralD(pNodoA *a);
-pNodoA *consultaABP(pNodoA *a, tipoinfo chave);
-pNodoA *consultaABP2(pNodoA *a, tipoinfo chave);
+pNodoA* consultaABP(pNodoA *a, tipoinfo chave);
+pNodoA* consultaABP2(pNodoA *a, tipoinfo chave);
--- BinTree/main.c
+++ BinTree/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,11 +14,11 @@ int main()
    arv1 = InsereArvore(arv1,90);
    arv1 = InsereArvore(arv1,80);
    arv1 = InsereArvore(arv1,95);
-   
+
    printf("--Pre Esquerda --\n");
    CentralE(arv1);
    printf("\n\n");
-    
+
    printf("--Pre Direita --\n");
    CentralD(arv1);
    printf("\n\n");
@@ -25,19 +26,19 @@ int main()
    printf("--Central Esquerda --\n");
    CentralE(arv1);
    printf("\n\n");
-    
+
    printf("--Central Direita --\n");
    CentralD(arv1);
    printf("\n\n");
-    
+
    printf("--Pos Esquerda --\n");
    posFixadoE(arv1);
    printf("\n\n");
-    
+
    printf("--Pos Direita --\n");
    posFixadoD(arv1);
    printf("\n\n");
-    
+
 }
 
 
